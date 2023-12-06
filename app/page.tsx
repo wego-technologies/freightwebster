c5ae66ff2dd259956facd7283c9e6191f136c996
adrian-wego <adrian@wegotech.io>
2023-12-06 14:57:26 -0600

    Window Sizing
    Made window the size of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,36 @@
 "use client"
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import React, { useState } from 'react';
+import React from 'react';
 import { Window, WindowContent, WindowHeader, Button, Toolbar } from 'react95';
 
 export default function Home() {
-  {/*const [terms, setTerms] = useState([]); // Replace with actual data fetching logic
-  const [selectedTerm, setSelectedTerm] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isFormVisible, setIsFormVisible] = useState(false);
-  const [newTerm, setNewTerm] = useState({ term: '', definition: '', sentence: '' });
-
- const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add logic to submit the new term
-    setIsFormVisible(false);
-  };
-
-  const filteredTerms = searchTerm.length === 0
-    ? terms
-: terms.filter(term => term.term.toLowerCase().includes(searchTerm.toLowerCase()));*/}
-
-
   return (
-    <main className={styles.main}>
+    <>
+      <style jsx global>{`
+        body, html {
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          height: 100%;
+          width: 100%;
+        }
+        #__next {
+          height: 100%;
+          overflow: hidden;
+        }
+      `}</style>
 
-
-      <Window style={{ width: '100%', height: '100vh' }}>
+      <Window style={{ width: '100%', height: '100%', boxSizing: 'border-box', margin: 0, padding: 0 }}>
         <WindowHeader>Freight Webster</WindowHeader>
-        <WindowContent>
+        <WindowContent style={{ height: 'calc(100% - 30px)', overflow: 'hidden' }}> {/* Adjust the height as per the header */}
           <Toolbar style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            {/*<SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />*/}
             <Button onClick={() => { }}>Add New Term</Button>
           </Toolbar>
-          <div style={{ display: 'flex', height: '100%' }}>
-            {/*<TermList terms={filteredTerms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-            <TermDetailPage selectedTerm={selectedTerm} />*/}
+          <div style={{ display: 'flex', height: '100%', overflow: 'hidden' }}>
+            {/* Content goes here */}
           </div>
-          {/*{isFormVisible && <AddTermForm handleSubmit={handleSubmit} newTerm={newTerm} setNewTerm={setNewTerm} setIsFormVisible={setIsFormVisible} />*/}
         </WindowContent>
       </Window>
-    </main>
-  )
+    </>
+  );
 }
