4564d6af0b329418b8b426e7b5fdc7c147418070
adrian-wego <adrian@wegotech.io>
2023-12-07 03:46:01 -0600

    Added Word List and Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,46 @@
 "use client"
 
 import React from 'react';
-import { Window, WindowContent, WindowHeader, Button, Toolbar } from 'react95';
+import { Window, WindowContent, WindowHeader, Button, Toolbar, TextInput, GroupBox, List, ListItem} from 'react95';
+import { createGlobalStyle } from 'styled-components';
+import { styleReset } from 'react95';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'MS';
+    font-style: regualr;
+    src: url('./fonts/ms-sans-serif-1.ttf') format('truetype');
+    src: url('./fonts/ms-sans-serif-1.otf') format('opentype');
+  }
+`;
 
 export default function Home() {
   return (
     <>
-      <style jsx global>{`
-        body, html {
-          margin: 0;
-          padding: 0;
-          overflow: hidden;
-          height: 100%;
-          width: 100%;
-        }
-        #__next {
-          height: 100%;
-          overflow: hidden;
-        }
-      `}</style>
-
-      <Window style={{ width: '100%', height: '100%', boxSizing: 'border-box', margin: 0, padding: 0 }}>
+      <GlobalStyles />
+      <Window style={{ fontFamily: 'MS', width: '100%', height: '100%', boxSizing: 'border-box', margin: 0, padding: 0 }}>
         <WindowHeader>Freight Webster</WindowHeader>
-        <WindowContent style={{ height: 'calc(100% - 30px)', overflow: 'hidden' }}> {/* Adjust the height as per the header */}
+        <WindowContent style={{ height: 'calc(100% - 30px)', overflow: 'auto' }}> {/* Adjust the height as per the header */}
           <Toolbar style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <TextInput placeholder="Search..." width={150} />
             <Button onClick={() => { }}>Add New Term</Button>
           </Toolbar>
-          <div style={{ display: 'flex', height: '100%', overflow: 'hidden' }}>
+            <GroupBox label='A'>
+              <ListItem onClick={() => { }}>Accessorial Charges</ListItem>
+              <ListItem onClick={() => { }}>Authority</ListItem>
+            </GroupBox>
+            <GroupBox label='B'>
+              <ListItem onClick={() => { }}>Bill of Lading (BOL)</ListItem>
+              <ListItem onClick={() => { }}>Bobtail</ListItem>
+            </GroupBox>
+            <GroupBox label='C'>
+              <ListItem onClick={() => { }}>Carrier</ListItem>
+              <ListItem onClick={() => { }}>Carrier Liability</ListItem>
+              <ListItem onClick={() => { }}>Consignee</ListItem>
+              <ListItem onClick={() => { }}>Consignor</ListItem>
+            </GroupBox>
+          <div style={{ display: 'flex', height: '100%', overflow: 'auto' }}>
             {/* Content goes here */}
           </div>
         </WindowContent>
@@ -34,3 +48,5 @@ export default function Home() {
     </>
   );
 }
+
+
