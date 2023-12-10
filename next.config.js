114e90076a3acc82d44bf9469a6a31ede35c5030
Dorcy Shema <hello@dorcis.com>
2023-12-09 20:45:04 -0500

    chore: last pr push

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    experimental: {
-      scrollRestoration: true,
-    },
-    compiler: { styledComponents: true },
-  }
+const nextConfig = {}
 const withFonts = require('next-fonts');
 const withTM = require('next-transpile-modules')([
     '@react95/core',
